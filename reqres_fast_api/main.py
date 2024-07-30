63e2c1aa02540c18c9ae88378fc67813be3c26b8
v.prets <v.prets@2gis.ru>
2024-07-30 13:45:09 +0700

    Add db

--- reqres_fast_api/main.py
+++ reqres_fast_api/main.py
@@ -1,9 +1,13 @@
+import dotenv
+
+dotenv.load_dotenv()
+
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
+from reqres_fast_api.database.engine import create_db_and_tables
 from routers import status, users
 import uvicorn
 import logging
-import users_load
 
 app = FastAPI()
 app.include_router(status.router)
@@ -12,9 +16,9 @@ add_pagination(app)
 
 logger = logging.getLogger(__name__)
 
-
 if __name__ == '__main__':
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
     logger.info('Started')
+    create_db_and_tables()
     uvicorn.run(app)
     logger.info('Finished')
--- reqres_fast_api/routers/status.py
+++ reqres_fast_api/routers/status.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter
 from http import HTTPStatus
 
 from reqres_fast_api.models.AppStatus import AppStatus
-from reqres_fast_api.users_load import users
+from reqres_fast_api.database.engine import check_availability
 
 router = APIRouter(prefix="/api/status")
 
 
 @router.get("/", status_code=HTTPStatus.OK)
 async def status() -> AppStatus:
-    return AppStatus(users=bool(users))
+    return AppStatus(database=check_availability())
 
 
